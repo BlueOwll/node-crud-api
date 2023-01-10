f5f9e04233091701f1d88ff64850e4e767c50da4
BlueOwll <blueowl.n@gmail.com>
2023-01-11 01:37:30 +0300

    feat: implement get user, pu user, delete user operations

--- src/models/database.ts
+++ src/models/database.ts
@@ -1,5 +1,8 @@
+import { IUser, IUserData } from './user.js';
+import { v4 as uuid, validate as isValid } from 'uuid';
+
 interface IDatabase {
-  [key: string]: []
+  users: IUser[]
 }
 
 export class DataBase {
@@ -12,15 +15,62 @@ export class DataBase {
   }
 
   public addUser(data: string) {
-    console.log(data);
-    const   newUser = JSON.parse(data);
-    if (newUser instanceof Error) {
-      console.error(newUser);
-      throw new Error('Incorrect data');
+    let newUserData: IUserData;
+    try {
+      newUserData = JSON.parse(data);     
+    }catch(err) {
+      throw new Error('Invalid input');
     }
-      
-    console.log(`adding user ${newUser}`);
-    console.log(newUser);
+    const newUser: IUser = {
+      id: uuid(),
+      username: newUserData.username,
+      age: newUserData.age,
+      hobbies: newUserData.hobbies
+    }
+    Object.values(newUser).forEach((value) => {
+      if (value === undefined) throw new Error(`Missing required fields`);
+    });
+    this.dataBase.users.push(newUser);
     return newUser;
+  }
+
+  public getUser(userId: string) {
+    if (isValid(userId)) {
+      return this.dataBase.users.find((item) => item.id === userId)
+    } 
+    throw new Error('Invalid user id');
+  }
+
+  public deleteUser(userId: string) {
+    let user: IUser;
+    if (isValid(userId)) {
+      const userIndex = this.dataBase.users.findIndex((item) => item.id === userId);
+      if (userIndex === -1) return undefined;
+      [user] = this.dataBase.users.splice(userIndex, 1);
+      return user;
+    } 
+    throw new Error('Invalid user id');
+  }
+
+  public putUser(userId: string, data: string) {
+    let newUserData: IUserData;
+    try {
+      newUserData = JSON.parse(data);     
+    }catch(err) {
+      throw new Error('Invalid input');
+    }
+    
+    if (isValid(userId)) {
+      const userIndex = this.dataBase.users.findIndex((item) => item.id === userId);
+      if (userIndex === -1) return undefined;
+      const user = {...this.dataBase.users[userIndex]};
+      user.username = newUserData.username === undefined ? null : newUserData.username;
+      user.age = newUserData.age === undefined ? null : newUserData.age;
+      user.hobbies = newUserData.hobbies === undefined ? null : newUserData.hobbies;
+      this.dataBase.users[userIndex] = user;   
+      console.log(user);   
+      return user;
+    } 
+    throw new Error('Invalid user id');
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,33 @@
-import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import http from 'http';
-import { v4 as uuid } from 'uuid';
-import { IUser } from './models/user.js';
-import { DataBase } from './models/database.js';
-
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import http from "http";
+import { v4 as uuid } from "uuid";
+import { IUser } from "./models/user.js";
+import { DataBase } from "./models/database.js";
 
 dotenv.config();
-
 
 //console.log(`Server starts on host = ${process.env.HOST} PORT = ${process.env.PORT}`);
 const PORT = Number(process.env.PORT);
 const HOST = process.env.HOST;
 enum METHODS {
-  get = 'GET',
-  post = 'POST',
-  update = 'UPDATE',
-  delete = 'DELETE'
-} 
+  get = "GET",
+  post = "POST",
+  put = "PUT",
+  delete = "DELETE",
+}
+
+enum CODES {
+  OK = 200,
+  CREATED = 201,
+  NOTFOUND = 404,
+  BADREQUEST = 400,
+}
+
+enum ERRORS {
+  NOTFOUNDRESOURSE = 'Resource not found',
+  NOTFOUNDUSER = 'User not found',
+
+}
 
 const dataBase = new DataBase();
 
@@ -24,40 +35,103 @@ const server = http.createServer((req, res) => {
   //res.writeHead(200, { 'Content-Type': 'application/json' });
   res.setHeader("Content-Type", "application/json");
   console.log(req.url);
-  let body = '';
-  switch (req.url) {
-    case '/api/users':
-      switch (req.method) {
-        case METHODS.get:
-          res.writeHead(200);
-          res.end(JSON.stringify(dataBase.getUsers()));
-          break;
-        case METHODS.post:
-          console.log('POST');          
-          req.on('data', function(data) {
-            body += data
-            console.log('Partial body: ' + body)
-          })
-          req.on('end', function() {
-            console.log('Body: ' + body);
-            try{
-              const newUser = dataBase.addUser(body);
-              res.writeHead(201);
-              res.end(JSON.stringify(newUser));
-            } catch (e) {
-              res.writeHead(400);
-              res.end(e);
-            }            
-          })
-          break;
-      }  
-      break;  
-    default:
-      res.writeHead(404);
-      res.end(JSON.stringify({error:"Resource not found"}));
+
+  if (req.url === "/api/users") {
+    let body = "";
+    switch (req.method) {
+      case METHODS.get:
+        res.writeHead(CODES.OK);
+        res.end(JSON.stringify(dataBase.getUsers()));
+        break;
+
+      case METHODS.post:
+        console.log("POST");
+        req.on("data", (data) => {
+          body += data;
+        });
+        req.on("end", function () {
+          try {
+            const newUser = dataBase.addUser(body);
+            res.writeHead(CODES.CREATED);
+            res.end(JSON.stringify(newUser));
+          } catch (e) {
+            res.writeHead(CODES.BADREQUEST);
+            res.end({ error: e.message });
+          }
+        });
+        break;
+
+      default:
+        res.writeHead(CODES.NOTFOUND);
+        res.end(JSON.stringify({ error: ERRORS.NOTFOUNDRESOURSE }));
+    }
+  } else if (req.url.startsWith("/api/users/")) {
+    const userid = req.url.replace("/api/users/", "");
+    let user: IUser;
+    let body = "";
+    switch (req.method) {
+      case METHODS.get:
+        try {
+          user = dataBase.getUser(userid);
+          if (user) {
+            res.writeHead(CODES.OK);
+            res.end(JSON.stringify(user));
+          } else {
+            res.writeHead(CODES.NOTFOUND);
+            res.end(JSON.stringify({ error: ERRORS.NOTFOUNDUSER }));
+          }
+        } catch (e) {
+          res.writeHead(CODES.BADREQUEST);
+          res.end({ error: e.message });
+        }
+
+        break;
+      case METHODS.put:
+        req.on("data", (data) => {
+          body += data;
+        });
+        req.on("end", () => {
+          try {
+            user = dataBase.putUser(userid, body);
+            if (user) {
+              res.writeHead(CODES.OK);
+              res.end(JSON.stringify(user));
+            } else {
+              res.writeHead(CODES.NOTFOUND);
+              res.end(JSON.stringify({ error: ERRORS.NOTFOUNDUSER }));
+            }
+          } catch (e) {
+            res.writeHead(CODES.BADREQUEST);
+            res.end({ error: e.message });
+          }
+        });
+        break;
+      case METHODS.delete:
+        try {
+          user = dataBase.deleteUser(userid);
+          if (user) {
+            res.writeHead(CODES.OK);
+            res.end(JSON.stringify(user));
+          } else {
+            res.writeHead(CODES.NOTFOUND);
+            res.end(JSON.stringify({ error: ERRORS.NOTFOUNDUSER }));
+          }
+        } catch (e) {
+          res.writeHead(CODES.BADREQUEST);
+          res.end({ error: e.message });
+        }
+        break;
+      default:
+        res.writeHead(CODES.NOTFOUND);
+        res.end(JSON.stringify({ error: ERRORS.NOTFOUNDRESOURSE }));
+        return;
+    }
+  } else {
+    res.writeHead(CODES.NOTFOUND);
+    res.end(JSON.stringify({ error: ERRORS.NOTFOUNDRESOURSE }));
   }
 });
 
-server.listen(PORT,HOST, () => {
-  console.log(`Server is running on http://${HOST}:${PORT}`)}
-  );
+server.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
+});
